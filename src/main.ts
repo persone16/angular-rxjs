f36aeb01fec1643cd509d1c4cd1046eca5f551ba
persone716 <persone716@gmail.com>
2023-04-10 17:04:19 +0300

    Working with subject

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import "./rxjs";
+// import "./rxjs";
+// import "./rxjs-canvas";
+
+import "./rxjs-subject";
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
