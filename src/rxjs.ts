390ce3888eb4a5ffac679189a6b0b79926172c1f
persone716 <persone716@gmail.com>
2023-04-10 10:28:32 +0300

    Change app.component.html

---
+++ src/rxjs.ts
@@ -0,0 +1,53 @@
+import { debounceTime, distinctUntilChanged, fromEvent, Observable } from "rxjs";
+import { map, catchError } from 'rxjs/operators'
+
+// #1 case
+// const search$ = new Observable<Event>(observer => {
+// 	console.log('Start in Observable');
+	
+// 	const search = document.getElementById("search");
+
+// 	if(!search) {
+// 		observer.error('Element does not exist');
+// 		return;
+// 	}
+	
+// 	search.addEventListener("input", event => {
+// 		observer.next(event);
+// 		// observer.complete();
+// 	})
+
+// 	console.log('End in Observable');
+// });
+
+// #2 case
+const input = document.getElementById('search');
+const el1: HTMLElement = input as HTMLElement;
+const search$: Observable<Event> = fromEvent<Event>(
+	el1, 'input'
+);
+
+search$
+	.pipe(
+		map(event => {
+			return (event.target as HTMLInputElement).value
+		}),
+		debounceTime(500),
+		map(value => value.length > 3 ? value : ''),
+		distinctUntilChanged()
+	)
+	.subscribe(value => {
+		console.log(value);
+	});
+
+// search$.subscribe({
+// 	next: value => {
+// 		console.log(value);
+// 	},
+// 	error: err => {
+// 		console.log(err);
+// 	},
+// 	complete: () => {
+// 		console.log('event end');
+// 	}
+// });
