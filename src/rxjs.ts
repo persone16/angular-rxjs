b11236e6d411e04dc18e8010be8bea4c2caddc37
persone716 <persone716@gmail.com>
2023-04-10 14:40:22 +0300

    Working with handling events on observable

--- src/rxjs.ts
+++ src/rxjs.ts
@@ -1,21 +1,41 @@
 import { debounceTime, distinctUntilChanged, fromEvent, Observable } from "rxjs";
-import { map, catchError } from 'rxjs/operators'
+import { map, catchError, takeUntil } from 'rxjs/operators'
 
 // #1 case 
 // const search$ = new Observable<Event>(observer => {
 // 	console.log('Start in Observable');
 	
 // 	const search = document.getElementById("search");
+// 	const stop = document.getElementById("stop");
 
-// 	if(!search) {
+// 	if(!search || !stop) {
 // 		observer.error('Element does not exist');
 // 		return;
 // 	}
-	
-// 	search.addEventListener("input", event => {
+
+// 	const onSearch = event => {
+// 		checkSubscription();
+// 		console.log(1234);
 // 		observer.next(event);
-// 		// observer.complete();
-// 	})
+// 	}
+// 	const onStop = event => {
+// 		observer.complete();
+// 		clear();
+// 	}
+
+// 	search.addEventListener("input", onSearch);
+// 	stop.addEventListener("click", onStop)
+
+// 	const checkSubscription = () => {
+// 		if (observer.closed) {
+// 			clear();
+// 		}
+// 	}
+
+// 	const clear = () => {
+// 		search.removeEventListener('input', onSearch);
+// 		search.removeEventListener('click', onStop);
+// 	}
 
 // 	console.log('End in Observable');
 // });
@@ -23,10 +43,13 @@ import { map, catchError } from 'rxjs/operators'
 // #2 case 
 const input = document.getElementById('search');
 const el1: HTMLElement = input as HTMLElement;
-const search$: Observable<Event> = fromEvent<Event>(
-	el1, 'input'
-);
+const search$: Observable<Event> = fromEvent<Event>(el1, 'input');
 
+const stop = document.getElementById('stop');
+const el2: HTMLElement = stop as HTMLElement;
+const stop$: Observable<Event> = fromEvent<Event>(el2, 'click');
+
+// const searchSubscription = 
 search$
 	.pipe(
 		map(event => {
@@ -34,11 +57,22 @@ search$
 		}),
 		debounceTime(500),
 		map(value => value.length > 3 ? value : ''),
-		distinctUntilChanged()
+		distinctUntilChanged(),
+		takeUntil(stop$)
 	)
 	.subscribe(value => {
 		console.log(value);
 	});
+
+// const stopSubscription = stop$.subscribe(() => {
+// 	searchSubscription.unsubscribe();
+// 	stopSubscription.unsubscribe();
+// });
+
+// setTimeout( () => {
+// 	console.log("Unsubscribed!")
+// 	searchSubscription.unsubscribe();
+// }, 10000)
 
 // search$.subscribe({
 // 	next: value => {
